e3e1ad0173005a684e268664a22070db53466d09
Stacie Jennifer <47727143+StacieJS@users.noreply.github.com>
2019-05-27 17:46:00 -0400

    Initial commit of magic8ball files

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+ 
+    var magic8Ball = {};
+    
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams!"];
+
+    $("#answer").hide();
+
+    
+   
+    magic8Ball.askQuestion = function(question){
+        $("#answer").fadeIn(4000);
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+        
+        $("#answer").text( answer );
+ 
+        console.log(question);
+        console.log(answer);
+    };
+
+    
+        var onClick = function() {
+        $("#answer").hide();
+        var question = prompt("Ask Me Anything!");
+        magic8Ball.askQuestion(question);
+    };
+ 
+
+        $("#questionButton").click( onClick );
+
+
+ 
+});
