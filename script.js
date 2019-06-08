a8c90cf69829a6f1ed2b2460b65f96bfa83123fa
Stacie Jennifer <47727143+StacieJS@users.noreply.github.com>
2019-06-08 14:48:16 -0400

    Added shake effect
    Line 30 and line 37

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ $(document).ready(function(){
         $("#answer").hide();
         $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
         var question = prompt("Ask Me Anything!");
+	$("#8ball").effect( "shake" );
         magic8Ball.askQuestion(question);
     };
  
 setTimeout(
 function() {
         var question = prompt("Ask Me Anything!");
+	$("#8ball").effect( "shake" );
         magic8Ball.askQuestion(question);
 		   }, 500);
  
 
-   $("#questionButton").click( onClick );
-        $("#questionButton").click( onClick );
-
-
+  	$("#questionButton").click( onClick );
+        
  
 });
